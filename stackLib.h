cc90df3f562167195379d9f4427762199609432b
Jorge Guerrero <89558483+GuerreroHJorge@users.noreply.github.com>
2021-10-19 12:06:59 -0500

    Create stackLib.h

---
+++ stackLib.h
@@ -0,0 +1,16 @@
+#ifndef STACKLIB_STACKLIB_H
+#define STACKLIB_STACKLIB_H
+
+typedef struct node Stack;
+typedef struct pila Pila;
+
+typedef void* TYPE;
+
+void push(Pila* pila, void* value);
+void* pop(Pila* pila);
+void* peek(Stack* stack);
+
+void printStack(Pila* stack);
+Stack* newStack();
+
+#endif
